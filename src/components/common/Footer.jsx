be33b7904ea1138b66c5bf1836976a9e612c9e2a
DogonYu <dorogon01@gmail.com>
2020-12-24 23:19:43 +0900

    푸터 mock

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <FooterWrap>
+      <div>푸터</div>
+      <Link to="instagram.com">인스타</Link>
+    </FooterWrap>
+  );
+};
+
+const FooterWrap = styled.div``;
+
+export default Footer;
