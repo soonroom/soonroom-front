e0ee8d4c0c7a262deffe88540fba67fb67465139
DogonYu <dorogon01@gmail.com>
2020-12-28 09:13:54 +0900

    기본 이미지

--- src/components/rooms/RoomCard.jsx
+++ src/components/rooms/RoomCard.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
+import defaultImg from '@assets/img/clustermarker1.png';
 
 const RoomList = ({ data }) => {
   return (
     <RoomCardWrap>
-      <div>이름: {data.name}</div>
-      <div>에어컨: {data.is_aircon ? '있음' : '없음'}</div>
-      <div>방세: {data.rent}</div>
+      <div className="img-box">
+        <img src={defaultImg} alt="" />
+      </div>
+      <div className="data-box">
+        <div>이름: {data.name}</div>
+        <div>에어컨: {data.is_aircon ? '있음' : '없음'}</div>
+        <div>방세: {data.rent}</div>
+      </div>
     </RoomCardWrap>
   );
 };
 
 const RoomCardWrap = styled.div`
+  display: flex;
   width: 100%;
   height: 150px;
   border: 1px solid ${oc.gray[4]};
@@ -20,6 +27,15 @@ const RoomCardWrap = styled.div`
   padding: 1rem;
   & + & {
     margin-top: 1rem;
+  }
+  .img-box {
+    display: flex;
+    align-items: center;
+    margin-right: 2rem;
+    img {
+      height: 120px;
+      width: 120px;
+    }
   }
 `;
 
